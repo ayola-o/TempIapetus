41e4667339a8def18d3ee5b81e4c0a4553e9c126
Ayola Oni <theayoolaoni@gmail.com>
2017-04-24 21:16:50 +0100

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,39 @@
-$(document).foundation()
+window.onload = function () {
 
-var questions = [{
-    number: 1,
-    question:"Please choose an appropriate sex:",
-    choices:["Male", "Female", "Bobrisky"],
-}, {
-    number: 2,
-    question:"What do you do for a living?",
-    choices:["Developer", "Designer", "Content Provider","Banker","Teacher","Doctor",,"Lawyer","Taxify/Uber Driver","Taxify/Uber Driver", "Intern at a startup in Yaba","Staying woke on twitter", "Alariwo", "I am a Nigerian Politician"],
-},{
-  number: 3,
-  question:"How much do you earn currently?",
-  choices:["10k-50k", "50k-100k", "100k-500k", "500k-1,000,000", "Nothing i am an intern getting paid in exposure", "I earn in dollars", "I am a Nigerian Politician"]
-},{
-  number:4,
-  question:"How many assets do you have?",
-  choices:["LOL", "1-3", "3-5", "5-10", "I am a Nigerian Politician"]
-},{
-  number:5,
-  question:"How much debt are you in right now?",
-  choices:["None", "Less than 100k", "100k - 500k", "500k - 1,000,000", "I am a Nigerian Politician"]
-},{
-  number:6,
-  question:"Are you single ?"
-  choices:["Yes", "No"]
-}];
+
+};
+
+var answer = [
+  {"name": "Bobrisky","image": "http://www.wetinhappen.com.ng/wp-content/uploads/2016/11/vllkyt74cdq2dlmqu.06100662.jpg", "Trait": "You go girl!! "
+  +"Keep tensioning people's daddies, hopefully they don't come for you soon."},
+  {"name": "Deola Sagoe","image": "http://blog.nigerianfabrics.com/wp-content/uploads/2014/10/Deola-Sagoe-1.jpg", "Trait": "A visionary that will be remembered in Nigerian history"
+  +"Keep doing you girl! We love you."},
+  {"name": "Mai Atafo","image": "http://allure.vanguardngr.com/wp-content/uploads/2016/04/Mai-Atafo-1-e1461575500868.jpg", "Trait": "Trail blazer!"
+  +"Thank you for being so amazing! We love you."},
+  {"name": "Mai Atafo","image": "http://allure.vanguardngr.com/wp-content/uploads/2016/04/Mai-Atafo-1-e1461575500868.jpg", "Trait": "Trail blazer!"
+  +"Thank you for being so amazing! We love you."},
+  {"name": "Mark Essien","image": "https://www.bellanaija.com/wp-content/uploads/2015/05/mark.jpg", "Trait": "First of name,  First of his kind. Dollaz general"
+  +"Keep building the future, And inspiring millions"},
+  {"name": "Ire Aderinokun","image": "http://grafrica.com.ng/wp-content/uploads/2017/03/ire-aderinokun.jpg", "Trait": "One word. Iconic"
+  +"Destroying stereotypes and slaying whilst at it"},
+  {"name": "Frank Donga","image": "https://static.pulse.ng/img/gist/origs2851918/3319721820-w900-h600/Frank-Donga-May-2014.png", "Trait": "FRANKly speaking we think you need to leave that startup"
+  +"There is no rule against getting exposure and making dollaz (shout out  Timigod)"
+  +"Cuz tbh if you continue like this, you will end up in Macdonalds"},
+  {"name": "Queen of the night","image": "http://4.bp.blogspot.com/-JALlJc7VqhI/VR2btBMlYKI/AAAAAAABpig/PXNO_3N4Ncc/s1600/pr.jpg", "Trait": "Olosho is your middle name. Home destroyer is the Third name"
+  +"Patiently Waiting for your next victim so you can destroy him and his family"
+  +"Karma is waiting for you aunty. Or a book deal."},
+  {"name": "Diezani Alison-Madueke","image": "http://www.36ng.com.ng/wp-content/uploads/2015/01/Diezani.jpg", "Trait": "Based on logistics, we cannot categorically calculate your networth"
+  +"But na the work wey you dey do go put you inside trouble"
+  +"We would be sending the EFCC in your direction soon"},
+  {"name": "Andrew Yakubu","image": "https://static.pulse.ng/img/incoming/origs6236310/1660481118-w980-h640/Andrew-Yakubu.jpg", "Trait": "You're the man already!"
+  +"But na the work wey you dey do go put you inside trouble"
+  +"We would be sending the EFCC in your direction soon"},
+  {"name": "Aliko Dangote","image": "https://www.bellanaija.com/wp-content/uploads/2014/01/Aliko-Dangote-Bloomberg-Billionaires-Index-January-2014-BellaNaija.jpg", "Trait": "Father of the Economy!"
+  +"H'appy weekend sir!"
+  +"Awon boyz yin loyal baje sir"}
+  {"name": "Folorunsho Alakija","image": "http://kingdomnews.ng/wp-content/uploads/2016/11/Alakija.jpg", "Trait": "H'appy new month ma!"
+  +"Loyalist la wa oh! "
+  +"Shake body for the boys"}
+  {"name": "Imogie Mubarak","image": "https://pbs.twimg.com/profile_images/689506244017553408/WsUM_qNb.jpg", "Trait": "La Flames"
+  +"Loyalist la wa oh! "}
+]
--- assets/js/stress.js
+++ assets/js/stress.js
@@ -1,17 +1,98 @@
-var Name = document.getElementById('nem')
-function newName (){
-  alert("Welcome " + Name.value);
+
+var person;
+var picture;
+var reccurent = "<h1 class = 'satisfied'>Not Satisfied?</h1>"+"<input type='button' id='restart' value = 'Try again'><br>"+
+"<h3 class = 'logo'>Geek<span id = 'dot'>.</span>Inc</h3>";
+
+$('#Q1').hide();
+$('#Q2').hide();
+$('#Q3').hide();
+$('#Q4').hide();
+$('#Q5').hide();
+$('#Q6').hide();
+$('#Q7').hide();
+$('#Answer').hide();
+
+$('#start').click(function () {
+  $('.Page1').hide();
+  $('#Q1').fadeIn(1200);
+});
+$('#next1').click(function (){
+  var form1 = $('#nem').val();
+  if (form1=='') {
+    alert ('FUCKING FILL IT IN');
+  } else {
+  $('#Q1').hide();
+  $('#Q2').fadeIn(1200);
+  var Name = document.getElementById('nem')
+  function newName (){
+    alert("Welcome " + Name.value);
+  }
+  newName();
 }
 });
-$(window).load(function(){
-   //to hide all the divs for the other question except the first one
-   //$('#id') or $('.class') -- depending on the one you used
-   $('#Q1').hide();
-   $('#Q2').hide();
-   $('#Q3').hide();
-   $('#Q4').hide();
-   $('#Q5').hide();
-   $('#Q6').hide();
-   $('#Q7').hide()
-   // and so on
+$('#prev1').click(function (){
+  $('#Q2').hide();
+  $('#Q1').fadeIn(1200);
+});
+$('#next2').click(function (){
+  $('#Q2').hide();
+  $('#Q3').fadeIn(1200);
+});
+$('#prev2').click(function (){
+  $('#Q3').hide();
+  $('#Q2').fadeIn(1200);
+});
+$('#next3').click(function (){
+  $('#Q3').hide();
+  $('#Q4').fadeIn(1200);
+});
+$('#prev3').click(function (){
+  $('#Q4').hide();
+  $('#Q3').fadeIn(1200);
+});
+$('#next4').click(function (){
+  $('#Q4').hide();
+  $('#Q5').fadeIn(1200);
+});
+$('#prev4').click(function (){
+  $('#Q5').hide();
+  $('#Q4').fadeIn(1200);
+});
+$('#next5').click(function (){
+  $('#Q5').hide();
+  $('#Q6').fadeIn(1200);
+});
+$('#prev5').click(function (){
+  $('#Q6').hide();
+  $('#Q5').fadeIn(1200);
+});
+$('#next6').click(function (){
+  $('#Q6').hide();
+  $('#Q7').fadeIn(1200);
+});
+$('#prev6').click(function (){
+  $('#Q7').hide();
+  $('#Q6').fadeIn(1200);
+});
+$('#finish').click(function (){
+  $('#Q7').hide();
+  $('#Answer').show(slow);
+  $('#Answer').fadeIn(1200);
+  $("#twitter").show("slow");
+  $("#twitter").fadeIn(1200);
+
+var selectedOption2 = $("input:radio[name='Q2']:checked").val();
+var selectedOption3 = $("input:radio[name='Q3']:checked").val();
+var selectedOption4 = $("input:radio[name='Q4']:checked").val();
+var selectedOption5 = $("input:radio[name='Q5']:checked").val();
+var selectedOption6 = $("input:radio[name='Q6']:checked").val();
+var selectedOption7 = $("input:radio[name='Q7']:checked").val();
+
+if (selectedOption2 == 'bobrisky') {
+
+} else {
+
+}
+
 });
